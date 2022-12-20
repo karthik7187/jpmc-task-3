3353b6511a553a8351a19893f00d4d7ff344f9f6
karthik <dattakarthik7@gmail.com>
2022-12-20 23:33:42 +0530

    committed again

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -39,6 +39,7 @@ class Graph extends Component<IProps, {}> {
         if (this.table) {
             // Load the `table` in the `<perspective-viewer>` DOM reference.
             elem.load(this.table);
+            elem.load(this.table);
             elem.setAttribute('view', 'y_line');
             elem.setAttribute('row-pivots', '["timestamp"]');
             elem.setAttribute('columns', '["ratio", "lower_bound", "upper_bound", "trigger_alert"]');
